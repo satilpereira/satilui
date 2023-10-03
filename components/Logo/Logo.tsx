36f94eeaecaca6acf7c8b06aa0703ecfc26a7b5e
satilpereira <satil.prog@gmail.com>
2023-10-03 09:17:34 -0300

    docs(comment): add a comment to identify a symbol

--- components/Logo/Logo.tsx
+++ components/Logo/Logo.tsx
@@ -9,6 +9,7 @@ const Logo = () => {
   return (
     <div className='h-12 w-12 hover:cursor-pointer'>
       <motion.svg height={48} width={48} viewBox='0 0 300 300'>
+        {/* Draws my S logo */}
         <motion.path
           d='M 266 266 L 266 37 L 37 37 L 37 266 L 189 266 L 189 189 L 113 189 L 113 113 L 189 113'
           fill='transparent'
